a047910242b145e52b78d48dd4cda9f70bd46681
Dumazert <rebeccadumazert@hotmail.fr>
2022-11-22 17:32:37 +0100

    ajoute favicon & librairie 'serve-favicon'

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,13 @@ import session from 'express-session';
 import helmet from "helmet";
 import path from "path";
 import demoRouter from "./routes";
+import favicon from "serve-favicon"
 
 const server = () => {
+    
     const app: Application = express();
 
+    app.use(favicon(path.join(__dirname, 'images', 'favicon-32x32.png')))
     app.set('views', path.join(__dirname, '../views'));
     app.set('view engine', 'ejs');
     app.use(express.static(path.join(__dirname,
